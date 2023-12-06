f75ba80c188a528e727e67239b5c16210873ee1a
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-06 22:06:20 +0700

    chuyen_doi_tien_te

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhap so tien ban muon doi");
+        int usd = Convert.ToInt32(Console.ReadLine());
+        int vnd = usd * 32000;
+        Console.WriteLine("vay tien VN la : "+vnd);
+}
+}
